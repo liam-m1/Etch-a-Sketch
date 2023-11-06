f2791441307c180b554964596270fbfd7643cf09
liam marchesi <147132249+liam-m1@users.noreply.github.com>
2023-11-05 21:36:50 -0800

    added loop to make divs for container, added feature to change div background color through change of class with mouse event, made div in grid 16x16 with flex basis

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
 console.log("js started");
 
-document.querySelector(".container");
+const container = document.querySelector(".container");
+let box = [];
+
+for (i = 0; i < 256; i++) {
+    const newBox = document.createElement("div");
+    container.appendChild(newBox);
+    box[i] = newBox;
+}
+
+box.forEach(box => {
+    box.addEventListener("mouseover", () => {
+    box.setAttribute("class", "boxOver")
+    });
+});
+
+});
