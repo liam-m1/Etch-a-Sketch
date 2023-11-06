56274cd3b12b5c5447546cf6b2be3e8c09c09032
liam marchesi <147132249+liam-m1@users.noreply.github.com>
2023-11-05 22:03:03 -0800

    added button that removes div class when clicked to reset board, stylized container

--- javascript.js
+++ javascript.js
@@ -4,15 +4,27 @@ console.log("js started");
 const container = document.querySelector(".container");
 let box = [];
 
+function boxSize(size) {
 for (i = 0; i < 256; i++) {
     const newBox = document.createElement("div");
     container.appendChild(newBox);
     box[i] = newBox;
 }
+}
+
+boxSize(16);
 
 box.forEach(box => {
     box.addEventListener("mouseover", () => {
     box.setAttribute("class", "boxOver")
+    });    
+});
+
+const buttonSize = document.querySelector("button");
+buttonSize.addEventListener("click", () => {
+    box.forEach(box => {
+    box.classList.remove("boxOver");
+
     });
 });
 
